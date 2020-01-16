f8e73e11fbdea425c38658000e2dc58bbd944a13
AverchenkoAA <aaaops1257@gmail.com>
2020-01-16 18:30:20 +0300

    Fix test

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,7 @@
-import {getAllComputers} from '../middleware/computers.js';
-import {app} from '../app.js/index.js.js';
-
-const server = app.listen();
-
-
+import {getAllComputers} from '../dist/middleware/computers.js';
+import {app} from '../dist/app.js';
 
 test('Check return getAllComputers()', () => {
   expect(getAllComputers()).toBe('All computers (func)');
 });
 
-test('adds 1 + 2 to equal 3', () => {
-  expect().toBe('All computers (func)');
-});
